d0972b7d6bd40c04bcae837511ae409d061b38ba
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-17 07:57:05 +0200

    Adding one block more to my first test so 'player can start game' before choosing a weapon. Importing header from Semantic. Creating a new folder to store components.

---
+++ rps/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from 'semantic-ui-react'
+
+const HeaderExamplePage = () => (
+  <div>
+    <Header as='h1'>Welcome to the Rock Scissors Paper's game!</Header>
+
+  </div>
+)
+
+export default HeaderExamplePage
